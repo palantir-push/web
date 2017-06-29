58cade0ac6fd40656bd2d171dcc47f720d1cf453
Trygve Amundsen <trygve.amundsen@gmail.com>
2017-06-29 12:44:14 +0200

    Remove react brand and add simple palantir-brand

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Palantir-push</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Just wait some hours and exciting things will start happening here...
         </p>
       </div>
     );
